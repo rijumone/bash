3785a5e7fadb228c1eafb7761300441b699857c3
Riju <mailmeonriju@gmail.com>
2016-12-28 20:20:28 +0530

    complete

---
+++ get_reports/format.sh
@@ -0,0 +1,35 @@
+if [ $1 ]; then
+	
+	# check if dir to store cookie reports exists
+	# if not then create it
+
+	if ! [ -d cookie_reports ]; then
+	  	mkdir -m 777 cookie_reports
+	fi
+
+	# now check if dated folder exists
+	# if not create
+
+	DIR=`date +%m%d%y`
+
+	if ! [ -d cookie_reports/$DIR ]; then
+	  	mkdir -m 777 cookie_reports/$DIR
+	fi
+
+	# get date attributes
+
+	month=`date +%b`
+	day=`date +%d`
+	year=`date +%Y`
+
+	value='{"startDate": "'$month' '$day' '$year' 00:00:00 GMT+0530 (India Standard Time)", "endDate": "'$month' '$day' '$year' 23:59:59 GMT+0530 (India Standard Time)"}'
+	
+	value='{"startDate": "Oct 30 2016 00:00:00 GMT+0530 (India Standard Time)", "endDate": "Oct 30 2016 06:59:59 GMT+0530 (India Standard Time)"}'
+
+	echo '+---------------------------------+\n| Saving report '$1' for '$month $day $year' |\n+---------------------------------+\n'
+
+	# make curl request and save to file
+
+	curl -H "Content-Type: application/json" -d "$value" http://localhost:1337/export/format"$1" -o cookie_reports/$DIR/$month"_"$day"_"$year.csv
+
+fi
